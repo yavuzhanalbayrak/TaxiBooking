7d218ea67b3c23d59a9ff07d1a8efd19efc1aa32
yavuzalbayrak[O <yavuz.albayrak@venhancer.com>
2024-04-20 21:23:57 +0300

    fix

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -7,15 +7,11 @@ import Map from "../../components/Map";
 import LocationInputs from "../../components/LocationInputs";
 
 export default function HomePage() {
-  const [source, setSource] = useState([]);
-  const [destination, setDestination] = useState([]);
-  //const {destination, setDestination, source, setSource} = useContext(GlobalContext);
-
 
   return (
     <Row gutter={[10,10]}>
       <Col span={24} xs={24} sm={24} md={7} lg={7} xl={7} xxl={7}>
-        <LocationInputs source={source} setSource={setSource}></LocationInputs>
+        <LocationInputs></LocationInputs>
       </Col>
       <Col span={24} xs={24} sm={24} md={17} lg={17} xl={17} xxl={17}>
         <Map></Map>
