615fa6472bcee135624b7ad91bf9bd842016eef4
yavuzalbayrak[O <yavuz.albayrak@venhancer.com>
2024-04-21 21:51:49 +0300

    inputs and road drawing done

--- frontend/src/components/LocationInputs.jsx
+++ frontend/src/components/LocationInputs.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Row, Col, Form, Input, Button, Dropdown } from "antd";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import location from "../images/location.png";
+import destination from "../images/currentLoc.png";
 
-export default function LocationInputs({setSource,setDestination}) {
-  const [startPoint, setStartPoint] = useState(null);
-  const [destPoint, setDestPoint] = useState(null);
+export default function LocationInputs({ setSource, setDestination }) {
+  const [focus, setFocus] = useState(false);
 
   const onSearch = () => {
     console.log("search");
@@ -36,6 +37,7 @@ export default function LocationInputs({setSource,setDestination}) {
       }
     });
   };
+
   return (
     <>
       <Row style={{ marginBottom: "5px" }}>
@@ -44,51 +46,99 @@ export default function LocationInputs({setSource,setDestination}) {
         </Col>
       </Row>
       <Row gutter={[0, 10]}>
-        <Col span={24}>
-          <GooglePlacesAutocomplete
-            selectProps={{
-              placeholder: "Başlangıç adresinizi giriniz",
-              destPoint,
-              onChange: (place, type) => {
-                type.name = "source";
-                setDestPoint();
-                getLatAndLng(place, type);
-              },
-              isClearable: true,
-              components: {
-                DropdownIndicator: false,
-              },
-              styles: {
-                control: (provided) => ({
-                  ...provided,
-                  width: "100%",
-                }),
-              },
-            }}
-          />{" "}
+        <Col
+          span={24}
+          style={{
+            backgroundColor: "#00000011",
+            paddingLeft: "10px",
+            borderRadius: "5px",
+            border: "1px solid #00000045",
+            borderColor: focus === 1 ? "black" : "#00000045",
+          }}
+        >
+          <Row align="middle">
+            <Col span={2}>
+              <img
+                style={{ width: "15px", height: "22px" }}
+                src={destination}
+                alt=""
+              />
+            </Col>
+            <Col span={22}>
+              <GooglePlacesAutocomplete
+                selectProps={{
+                  placeholder: "Başlangıç adresinizi giriniz",
+                  onFocus: () => setFocus(1),
+                  onBlur: () => setFocus(false),
+                  onChange: (place, type) => {
+                    type.name = "source";
+                    getLatAndLng(place, type);
+                  },
+                  isClearable: true,
+                  components: {
+                    DropdownIndicator: false,
+                  },
+                  styles: {
+                    control: (provided) => ({
+                      ...provided,
+                      background: "transparent", // Set background to transparent
+                      border: "none", // Remove border
+                      boxShadow: "none", // Remove box shadow if any
+                      width: "100%",
+                      cursor: "text",
+                    }),
+                  },
+                }}
+              />{" "}
+            </Col>
+          </Row>
         </Col>
-        <Col span={24}>
-          <GooglePlacesAutocomplete
-            selectProps={{
-              placeholder: "Varış adresinizi giriniz",
-              startPoint,
-              onChange: (place, type) => {
-                type.name = "source";
-                setDestPoint();
-                getLatAndLng(place, type);
-              },
-              isClearable: true,
-              components: {
-                DropdownIndicator: false,
-              },
-              styles: {
-                control: (provided) => ({
-                  ...provided,
-                  width: "100%",
-                }),
-              },
-            }}
-          />{" "}
+        <Col
+          style={{
+            backgroundColor: "#00000011",
+            paddingLeft: "10px",
+            borderRadius: "5px",
+            border: "1px solid #00000045",
+            borderColor: focus === 2 ? "black" : "#00000045",
+          }}
+          span={24}
+        >
+          <Row align="middle">
+            <Col span={2}>
+              <img
+                style={{ width: "15px", height: "22px" }}
+                src={location}
+                alt=""
+              />
+            </Col>
+            <Col span={22}>
+              <GooglePlacesAutocomplete
+                selectProps={{
+                  placeholder: "Varış adresinizi giriniz",
+                  onFocus: () => setFocus(2),
+                  onBlur: () => setFocus(false),
+                  onChange: (place, type) => {
+                    type.name = "destination";
+                    getLatAndLng(place, type);
+                  },
+                  isClearable: true,
+                  components: {
+                    DropdownIndicator: false,
+                  },
+                  styles: {
+                    control: (provided) => ({
+                      ...provided,
+                      background: "transparent", // Set background to transparent
+                      border: "none", // Remove border
+                      boxShadow: "none", // Remove box shadow if any
+                      width: "100%",
+                      cursor: "text",
+                    }),
+                  },
+                }}
+              />
+            </Col>
+          </Row>
         </Col>
         <Col span={24}>
           <Button
--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
-import locationPng from "../images/location.png"
+import {
+  GoogleMap,
+  MarkerF,
+  useJsApiLoader,
+  OverlayView,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import currentLocation from "../images/currentLoc.png";
+import location from "../images/location.png";
 
 const containerStyle = {
   width: "100%",
@@ -9,6 +16,8 @@ const containerStyle = {
 
 function Map({ source, destination }) {
   const [map, setMap] = React.useState(null);
+  const [directionRoutePoints, setDirectionRoutePoints] = useState([]);
+
   const [center, setCenter] = useState({
     lat: 41.288,
     lng: 36.333,
@@ -18,13 +27,49 @@ function Map({ source, destination }) {
     if (source?.length != []) {
       setCenter({ lat: source.lat, lng: source.lng });
     }
+
+    if (source?.length != [] && destination?.length != []) {
+      directionRoute();
+    }
   }, [source]);
 
   useEffect(() => {
-    if (destination) {
+    if (destination?.length != []) {
       setCenter({ lat: destination.lat, lng: destination.lng });
     }
+
+    if (source?.length != [] && destination?.length != []) {
+      directionRoute();
+    }
   }, [destination]);
+
+  const directionRoute = () => {
+    const DirectionsService = new google.maps.DirectionsService();
+
+    DirectionsService.route(
+      {
+        origin: {
+          lat: source.lat,
+          lng: source.lng,
+        },
+        destination: {
+          lat: destination.lat,
+          lng: destination.lng,
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result) => {
+        console.log("RES: ", result.status);
+
+        if (result.status === "OK") {
+          console.log("RES: ", result);
+          setDirectionRoutePoints(result);
+        } else {
+          console.log("Error");
+        }
+      }
+    );
+  };
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
@@ -48,15 +93,78 @@ function Map({ source, destination }) {
     >
       {source.length != [] ? (
         <MarkerF
-        icon={{
-          url:locationPng,
-          scaledSize:{
-            width: 30,
-            height: 30,
-          }
-        }}
-        position={{ lat: source.lat, lng: source.lng }} />
+          icon={{
+            url: currentLocation,
+            scaledSize: {
+              width: 17,
+              height: 22,
+            },
+          }}
+          position={{ lat: source.lat, lng: source.lng }}
+        >
+          <OverlayView
+            position={{ lat: source.lat, lng: source.lng }}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <div
+              style={{
+                backgroundColor: "#FFFFFFd5",
+                width: "80px",
+                fontSize: "15px",
+                textAlign: "center",
+                color: "black",
+                borderRadius: "10px",
+                boxShadow: "1.5px 1.5px 5px 0px",
+              }}
+            >
+              <p> {source.label} </p>
+            </div>
+          </OverlayView>
+        </MarkerF>
       ) : null}
+
+      {destination.length != [] ? (
+        <MarkerF
+          icon={{
+            url: location,
+            scaledSize: {
+              width: 17,
+              height: 22,
+            },
+          }}
+          position={{ lat: destination.lat, lng: destination.lng }}
+        >
+          <OverlayView
+            position={{ lat: destination.lat, lng: destination.lng }}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <div
+              style={{
+                backgroundColor: "#FFFFFFd5",
+                width: "80px",
+                fontSize: "15px",
+                textAlign: "center",
+                color: "black",
+                borderRadius: "10px",
+                boxShadow: "1.5px 1.5px 5px 0px",
+              }}
+            >
+              <p> {destination.label} </p>
+            </div>
+          </OverlayView>
+        </MarkerF>
+      ) : null}
+      <DirectionsRenderer
+        directions={directionRoutePoints}
+        options={{
+          polylineOptions: {
+            strokeColor: "blue",
+            strokeOpacity: 0.8,
+            strokeWeight: 5,
+          },
+          suppressMarkers: true,
+        }}
+      />
     </GoogleMap>
   );
 }
