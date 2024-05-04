cd035ed56cee5fd743419b0fdcf9ddf0598628cf
yavuzalbayrak[O <yavuz.albayrak@venhancer.com>
2024-05-04 17:24:20 +0300

    map loading error fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,24 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { LoadScript } from "@react-google-maps/api";
-import { LoadingOutlined } from "@ant-design/icons";
 
-const CustomLoadingElement = () => (
-  <div style={{ textAlign: "center", paddingTop: "20px", height:"100vh",alignContent:"center" }}>
-    <p>Harita Yükleniyor</p> <LoadingOutlined></LoadingOutlined>
-  </div>
-);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoadScript
-      googleMapsApiKey={import.meta.env.VITE_MAP_API}
-      libraries={["places"]}
-      loadingElement={<CustomLoadingElement />} // Set custom loading element
-
-    >
       <App />
-    </LoadScript>
   </React.StrictMode>
 );
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Row, Col, Card, Modal, Button } from "antd";
 import Map from "../../components/Map";
 import LocationInputs from "../../components/LocationInputs";
 import LoadingModal from "../../components/loadingModal/LoadingModal";
+import { useJsApiLoader } from "@react-google-maps/api";
+import { LoadingOutlined } from "@ant-design/icons";
+import GlobalContext from "../../context/GlobalContext";
 
 export default function HomePage() {
   const [source, setSource] = useState("");
@@ -10,11 +13,6 @@ export default function HomePage() {
   const [distance, setDistance] = useState("");
   const [isPhone, setIsPhone] = useState(window.innerWidth <= 768);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isScriptLoaded, setScriptLoaded] = useState(false);
-
-  const handleScriptLoad = () => {
-    setScriptLoaded(true);
-  };
 
   useEffect(() => {
     function handleResize() {
@@ -33,73 +31,95 @@ export default function HomePage() {
       setIsModalOpen(true);
     }
   };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_MAP_API,
+    libraries: ["places"],
+  });
+
   return (
     <>
-        {isPhone ? (
-          //Phone
-          <Row gutter={[10, 10]}>
-            <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24} xxl={17}>
-              <div className="map">
-                <Card bordered={false} style={{ borderRadius: "0px" }}>
-                  <div style={{ position: "relative" }}>
+      {isLoaded ? (
+        <div>
+          {isPhone ? (
+            //Phone
+
+            <Row gutter={[10, 10]}>
+              <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24} xxl={17}>
+                <div className="map">
+                  <Card bordered={false} style={{ borderRadius: "0px" }}>
+                    <div style={{ position: "relative" }}>
+                      <Map
+                        source={source}
+                        destination={destination}
+                        setDistance={setDistance}
+                        distance={distance}
+                        isPhone={true}
+                      ></Map>
+                    </div>
+                    <div className="location-inputs-phone ">
+                      <Card
+                        className="location-inputs-card-phone"
+                        title="Varış Noktası Seçiniz"
+                      >
+                        <LocationInputs
+                          setSource={setSource}
+                          setDestination={setDestination}
+                          destination={destination}
+                          source={source}
+                          distance={distance}
+                          startSearchForDriver={startSearchForDriver}
+                        />
+                      </Card>
+                    </div>
+                  </Card>
+                </div>
+              </Col>
+            </Row>
+          ) : (
+            <Row style={{ padding: "24px" }} gutter={[10, 10]}>
+              <Col span={24} xs={24} sm={24} md={10} lg={10} xl={8} xxl={7}>
+                <Col>
+                  <Card title="Varış Noktası Seçiniz">
+                    <LocationInputs
+                      setSource={setSource}
+                      setDestination={setDestination}
+                      destination={destination}
+                      source={source}
+                      distance={distance}
+                      startSearchForDriver={startSearchForDriver}
+                    ></LocationInputs>
+                  </Card>
+                </Col>
+              </Col>
+              <Col span={24} xs={24} sm={24} md={14} lg={14} xl={16} xxl={17}>
+                <div className="map">
+                  <Card title="Harita" bordered={false}>
                     <Map
                       source={source}
                       destination={destination}
                       setDistance={setDistance}
                       distance={distance}
-                      isPhone={true}
+                      isPhone={false}
                     ></Map>
-                  </div>
-                  <div className="location-inputs-phone ">
-                    <Card
-                      className="location-inputs-card-phone"
-                      title="Varış Noktası Seçiniz"
-                    >
-                      <LocationInputs
-                        setSource={setSource}
-                        setDestination={setDestination}
-                        destination={destination}
-                        source={source}
-                        distance={distance}
-                        startSearchForDriver={startSearchForDriver}
-                      />
-                    </Card>
-                  </div>
-                </Card>
-              </div>
-            </Col>
-          </Row>
-        ) : (
-          <Row style={{ padding: "24px" }} gutter={[10, 10]}>
-            <Col span={24} xs={24} sm={24} md={10} lg={10} xl={8} xxl={7}>
-              <Col>
-                <Card title="Varış Noktası Seçiniz">
-                  <LocationInputs
-                    setSource={setSource}
-                    setDestination={setDestination}
-                    destination={destination}
-                    source={source}
-                    distance={distance}
-                    startSearchForDriver={startSearchForDriver}
-                  ></LocationInputs>
-                </Card>
+                  </Card>
+                </div>
               </Col>
-            </Col>
-            <Col span={24} xs={24} sm={24} md={14} lg={14} xl={16} xxl={17}>
-              <div className="map">
-                <Card title="Harita" bordered={false}>
-                  <Map
-                    source={source}
-                    destination={destination}
-                    setDistance={setDistance}
-                    distance={distance}
-                    isPhone={false}
-                  ></Map>
-                </Card>
-              </div>
-            </Col>
-          </Row>
-        )}
+            </Row>
+          )}
+        </div>
+      ):(<div
+        style={{
+          textAlign: "center",
+          paddingTop: "20px",
+          height: "100vh",
+          alignContent: "center",
+        }}
+      >
+        <p>Harita Yükleniyor</p> <LoadingOutlined></LoadingOutlined>
+      </div>)}
+
       <LoadingModal
         setIsModalOpen={setIsModalOpen}
         isModalOpen={isModalOpen}
