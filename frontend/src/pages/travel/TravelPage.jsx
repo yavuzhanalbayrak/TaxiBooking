4ad8c920ee57712cff6e200c9af701c03146fa11
yavuzalbayrak[O <yavuz.albayrak@venhancer.com>
2024-05-06 11:32:36 +0300

    rate driver added

--- frontend/src/pages/travel/TravelPage.jsx
+++ frontend/src/pages/travel/TravelPage.jsx
@@ -7,6 +7,7 @@ import {
   CarOutlined,
   StarOutlined,
   StarFilled,
+  CheckCircleOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
@@ -16,7 +17,14 @@ export default function TravelPage() {
 
   const totalStars = 5;
   const filledStars = Math.max(0, Math.min(driver?.rating || 0, totalStars));
-const emptyStars = totalStars - filledStars;
+  const emptyStars = totalStars - filledStars;
+  const [isTravelFinished, setIsTravelFinished] = React.useState(true);
+  const [driverRate, setDriverRate] = React.useState(
+    Math.max(0, Math.min(0, totalStars))
+  );
+  const [emptyStarsForDriverRate, setEmptyStarsForDriverRate] = React.useState(
+    Math.max(0, Math.min(5, totalStars))
+  );
 
   React.useEffect(() => {
     setSelectedKeys(["2"]);
@@ -31,14 +39,54 @@ const emptyStars = totalStars - filledStars;
               title={
                 <div style={{ textAlign: "center" }}>
                   {driver
-                    ? "Yolculuk Bilgileri"
+                    ? isTravelFinished?<div>
+                      <CheckCircleOutlined
+                        style={{ fontSize: "24px", color: "green",paddingTop:"20px" }}
+                      />
+                      <p style={{paddingBottom:"20px"}}>Yolculuk Tamamlandı</p>
+                    </div>:"Yolculuk Bilgileri"
                     : "Henüz Bir Yolculuk Başlatmadınız!"}
                 </div>
               }
               style={{ width: "100%" }}
             >
               {driver ? (
-                <Row>
+                <Row gutter={[0, 5]}>
+                  {isTravelFinished && (
+                    <Col span={24} style={{ textAlign: "center" }}>
+                     
+                      <p style={{fontSize:"20px"}}>Sürücüyü Puanla</p>
+                        
+                      <p style={{ paddingBottom: "20px", fontSize:"30px" }}>
+                        {[...Array(driverRate)].map((_, index) => (
+                          <StarFilled
+                            onClick={() => {
+                              setDriverRate(index + 1);
+                              setEmptyStarsForDriverRate(
+                                totalStars - (index + 1)
+                              );
+                            }}
+                            style={{ color: "#ffc800" }}
+                            key={index}
+                          />
+                        ))}
+                        {[...Array(emptyStarsForDriverRate)].map((_, index) => (
+                          <StarOutlined
+                            onClick={() => {
+                              setDriverRate(
+                                (prevState) => prevState + index + 1
+                              );
+                              setEmptyStarsForDriverRate(
+                                emptyStarsForDriverRate - (index + 1)
+                              );
+                            }}
+                            key={index}
+                          />
+                        ))}
+                      </p>
+                     
+                    </Col>
+                  )}
                   <Col span={24}>Adı: {driver?.name}</Col>
                   <Col span={24}>Soyadı: {driver?.surname}</Col>
                   <Col span={24}>E-Postası: {driver?.email}</Col>
@@ -56,6 +104,15 @@ const emptyStars = totalStars - filledStars;
                         <StarOutlined key={index} />
                       ))}
                     </span>
+                  </Col>
+                  <Col span={24} style={{ paddingTop: "20px" }}>
+                    {isTravelFinished ? (
+                      <Col span={24} style={{ textAlign: "center" }}>
+                        <Button style={{ width: "100%", height:"40px" }}>Ödeme Yap</Button>
+                      </Col>
+                    ) : (
+                      <Button style={{ width: "100%" }}>İptal Et</Button>
+                    )}
                   </Col>
                 </Row>
               ) : (
