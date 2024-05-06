19c13a392bf505952c96eb328d69bd581355dd7b
yavuzalbayrak[O <yavuz.albayrak@venhancer.com>
2024-05-07 02:47:14 +0300

    location added to profile for test

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,15 +1,52 @@
 import { Avatar, Col, Row } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import { UserOutlined, HomeOutlined } from "@ant-design/icons";
 import "./Profile.scss";
 
 export default function ProfilePage() {
+  const [lat, setLat] = React.useState("");
+  const [lng, setLng] = React.useState("");
+
+  useEffect(() => {
+    // Check if the Geolocation API is supported by the browser
+    if ("geolocation" in navigator) {
+      // Request the user's location
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Access the user's latitude and longitude
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+
+          // Now you can use the latitude and longitude to perform location-based tasks
+          console.log("Latitude:", position.coords.latitude);
+          console.log("Longitude:", position.coords.longitude);
+
+          // You might want to send this information to your server for further processing
+        },
+        (error) => {
+          // Handle errors if location retrieval fails
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      // Geolocation is not supported by the browser
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <Row justify="center">
       <Col className="" span={16} sm={24} md={16}>
         <Col className="profile-card">
           <Row>
-            <Col className="profile-image" span={8} sm={24} md={24} lg={12} xl={8}>
+            <Col
+              className="profile-image"
+              span={8}
+              sm={24}
+              md={24}
+              lg={12}
+              xl={8}
+            >
               <Avatar
                 style={{ height: "25vh", width: "25vh", fontSize: "20vh" }}
                 size="large"
@@ -33,16 +70,20 @@ export default function ProfilePage() {
                     Telefon Numarası:
                   </Col>
                   <Col className="profile-field" span={24}>
-                    Adres:
+                    Adres Lat:{lat}
+                  </Col>
+                  <Col className="profile-field" span={24}>
+                    Adres Lng:{lng}
                   </Col>
                 </Row>
               </Col>
             </Col>
           </Row>
         </Col>
-        <Row >
-
-        <Col className="profile-card" span={24}>asdas</Col>
+        <Row>
+          <Col className="profile-card" span={24}>
+            asdas
+          </Col>
         </Row>
       </Col>
     </Row>
